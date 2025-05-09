7f44aa552c39aa4f7d9537039aec2ee0d1304e4d
James Hu <lei.hu@osr-tech.com>
2025-05-09 20:13:42 +0800

    update nvim snippets

--- nvim/install.bat
+++ nvim/install.bat
@@ -2,4 +2,7 @@ rd /S /Q %LOCALAPPDATA%\nvim
 REM rd /S /Q %LOCALAPPDATA%\nvim-data
 
 mklink /D %LOCALAPPDATA%\nvim %~dp0\nvim
+
+mklink /D %LOCALAPPDATA%\nvim\snippets %~dp0\..\vscode_user\snippets
+
 REM mklink /D %LOCALAPPDATA%\nvim-data %~dp0\nvim-data
