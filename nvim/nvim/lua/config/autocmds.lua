a9072762c9816d84685a76d89e2e60e90295d619
James Hu <huleiak47@gmail.com>
2025-05-17 11:11:40 +0800

    add markdownlint config

--- nvim/nvim/lua/config/autocmds.lua
+++ nvim/nvim/lua/config/autocmds.lua
@@ -44,7 +44,7 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "json", "yaml", "lua" },
+  pattern = { "json", "yaml", "lua", "jsonc" },
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 2
