49bca85d591916ffb932fc7fae81f1f5c9d5d4fb
James Hu <lei.hu@osr-tech.com>
2025-08-14 11:48:30 +0800

    nvim增加C代码使用//进行格式化配置

---
+++ nvim/nvim/lua/plugins/comment.lua
@@ -0,0 +1,16 @@
+return {
+  "echasnovski/mini.comment",
+  opts = {
+    -- 覆盖/追加注释符号
+    options = {
+      custom_commentstring = function()
+        -- 只对 C 文件生效
+        if vim.bo.filetype == "c" then
+          return "// %s"
+        end
+        -- 其他文件类型保持默认
+        return nil
+      end,
+    },
+  },
+}
